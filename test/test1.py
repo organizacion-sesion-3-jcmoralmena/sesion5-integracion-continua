0ee2bedb128c52ab8198bc149499167b78c59f6e
Carlos Moral <jc.moralmena@edu.gva.es>
2023-04-13 17:40:23 +0200

    Proyecto suma python

---
+++ test/test1.py
@@ -0,0 +1,25 @@
+import unittest
+import sys
+ 
+# setting path
+sys.path.append('../')
+
+
+from funciones import es_par
+
+class TestPar(unittest.TestCase):
+
+    def test_par(self):
+        self.assertTrue(es_par(2))
+        
+    def test_par2(self):
+        self.assertTrue(es_par(2*2))
+        
+    def test_impar(self):
+        self.assertFalse(es_par(5))
+        
+    def test_impar2(self):
+        self.assertFalse(es_par(5*5))
+        
+if __name__ == '__main__':
+    unittest.main()
